4792df39cf068103717e7bffff1f3818dcd8b0d3
ROCKANK <149751932+ROCKANK@users.noreply.github.com>
2026-03-06 19:55:34 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const predictDiabetes = async (features) => {
-  const res = await axios.post("https://diabetes-predictor-1-7ue9.onrender.com/", {
+  const res = await axios.post("https://diabetes-predictor-1-7ue9.onrender.com/predict", {
     features: features,
   });
   return res.data;
